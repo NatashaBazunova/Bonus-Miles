ae8df473257a92ca9866d9ed61ece430a17ff24c
NatashaBazunova <natashaiosifova@mail.ru>
2022-02-02 16:30:56 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //данные
+        int ticketPrice = 2159;
+        int oneBonusMile = 20;
+        int yourBonusMile = ticketPrice / oneBonusMile;
+        System.out.println("Ваши бонусные мили:" + yourBonusMile);
+    }
+}
